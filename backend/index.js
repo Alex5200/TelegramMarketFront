a96ade0f4a5713e7f984ada629bede1293bac820
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2022-10-06 19:58:28 +0300

    add ho open window market

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const token = '5757735968:AAEePWgh0W1NN3sMEQwx6DbTrehtkldPJBk';
 
 const bot = new TelegramBot(token, {polling: true});
 
-const webUrl = "https://ya.ru";
+const webUrl = "https://cute-brigadeiros-89d50f.netlify.app/";
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
@@ -12,15 +12,16 @@ bot.on('message', async (msg) => {
 
 
   if(text === "/start"){
-    await bot.sendMessage( chatId, 'Ниже появиться ассортимент заполняйте форму ))', {
+    await bot.sendMessage( chatId, 'Ниже появиться кнопка с форомой для заказа', {
         reply_markup: {
             keyboard: [
-                [{text: "Заполниет форму"}]
+                [{text: "Заполниет форму",
+                    web_app: {url: webUrl}}]
             ]
         }
     });
 
-    await bot.sendMessage( chatId, 'Ниже появиться ассортимент заполняйте форму ))', {
+    await bot.sendMessage( chatId, 'Ниже появиться кнопка для заказа', {
         reply_markup: {
             inline_keyboard: [
                 [{text: "Сделать заказ", 
