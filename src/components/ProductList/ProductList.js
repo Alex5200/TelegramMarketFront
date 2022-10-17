b611e4fe667087a9d8c2248d9f0cfe07b4c3a018
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2022-10-17 20:20:25 +0300

    add data logic

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import './ProductList.css'
 import ProdictItem from "../ProductItems/ProdictItem";
+import {useTelegram} from "../../hooks/useTelegram";
 
 const product = [
     {id: '1', title: "Джинсы", price: 5000, description: 'Синий цвет, прямые'},
@@ -12,7 +13,39 @@ const product = [
     {id: '7', title: "Джинсы", price: 5000, description: 'Синий цвет, прямые'},
     {id: '8', title: "Джинсы", price: 5000, description: 'Синий цвет, прямые'},
 ]
+
+const getTotalPrice = (items = []) =>{
+    return items.reduce((acc, item) => {
+        return acc += item.price;
+    }, 0)
+}
+
 const ProductList = () =>{
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+
+    const onAdd = (product) =>{
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        }else{
+            newItems = [...addedItems, product];
+        }
+        setAddedItems(newItems)
+
+        if(newItems.length === 0){
+            tg.MainButton.hide();
+        }else{
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            });
+
+        }
+    }
+
     return(
         <div className={'list'}>
             {products.map(item=> (
