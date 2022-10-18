b25a27a82321b0a65eb8333e10aff4d2ffc56e1c
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2022-10-18 13:41:30 +0300

    add data logic

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,16 +6,19 @@ const Form = () => {
     const [contry, setContry] = useState('');
     const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physical');
+    const [email, setEmail] = useState('');
+
     const {tg} = useTelegram();
 
     const onSendData = useCallback(() =>{
         const data = {
             contry,
             street,
-            subject
+            subject,
+            email
         }
         tg.sendData(JSON.stringify(data));
-    }, [contry, street, subject])
+    }, [contry, street, subject, email])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
@@ -41,12 +44,16 @@ const Form = () => {
     const onChangeCountry = (e) =>{setContry(e.target.value)}
     const onChangesStreet = (e) =>{setStreet(e.target.value)}
     const onChangeSubject = (e) =>{setSubject(e.target.value)}
+    const onChangeEmail = (e) =>{setEmail(e.target.value)}
 
     return (
         <div className={"form"}>
             <h3>Ваедите ваши данные</h3>
             <input className={"input"} type="text" placeholder={"Страна"} value={contry} onChange={onChangeCountry}/>
             <input className={"input"} type="text" placeholder={"Улица"} value={street} onChange={onChangesStreet}/>
+
+            <input className={"input"} type="text" placeholder={"Email"} value={email} onChange={onChangeEmail}/>
+
             <select value={subject} onChange={onChangeSubject} className={"select"}>
                 <option value={"physical"}>Физ. лицо</option>
                 <option value={"legal"}>Юр. лицо</option>
